0336dbed289852236b526d42725dc20e1ae36a79
Jonathan Brown <jbrown@acuity.social>
2024-02-27 13:35:05 +0700

    Implment size_on_disk()

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+use futures_util::{SinkExt, StreamExt};
+use serde::{Deserialize, Serialize};
 use tokio::net::TcpStream;
-use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
+use tokio_tungstenite::{
+    connect_async, tungstenite::protocol::Message, MaybeTlsStream, WebSocketStream,
+};
 
 pub struct Index {
     ws_stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
@@ -12,4 +16,41 @@ impl Index {
         let index = Index { ws_stream };
         index
     }
+
+    pub async fn size_on_disk(&mut self) -> u64 {
+        let msg = RequestMessage::SizeOnDisk;
+        let json = serde_json::to_string(&msg).unwrap();
+        let _ = self.ws_stream.send(Message::Text(json)).await;
+        let msg = self.ws_stream.next().await.unwrap().unwrap();
+        let response: ResponseMessage = serde_json::from_str(msg.to_text().unwrap()).unwrap();
+
+        match response {
+            ResponseMessage::SizeOnDisk(size) => size,
+            _ => 0,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "type")]
+pub enum RequestMessage {
+    Status,
+    Variants,
+    SizeOnDisk,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(tag = "type", content = "data")]
+#[serde(rename_all = "camelCase")]
+pub enum ResponseMessage {
+    #[serde(rename_all = "camelCase")]
+    Status {
+        last_head_block: u32,
+        last_batch_block: u32,
+        batch_indexing_complete: bool,
+    },
+    Subscribed,
+    Unsubscribed,
+    SizeOnDisk(u64),
+    //    Error,
 }
