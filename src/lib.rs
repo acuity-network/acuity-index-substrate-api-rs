247530c10c8d7db2a5b3170f95ebf030f7cb5ed5
Jonathan Brown <jbrown@acuity.social>
2024-02-27 09:52:19 +0700

    Connect to indexer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
+
+pub struct Index {
+    ws_stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Index {
+    pub async fn connect(url: String) -> Self {
+        let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+        println!("WebSocket handshake has been successfully completed");
+        let index = Index { ws_stream };
+        index
     }
 }
